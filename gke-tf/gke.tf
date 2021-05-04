af56b1dbe7c988755d73ca709f522295e7a3809f
shreyasgune <sgune@protonmail.com>
2021-05-03 22:17:03 -0400

    BLIZZ-gke - minor edit 4

--- gke-tf/gke.tf
+++ gke-tf/gke.tf
@@ -1,77 +1,81 @@
-module "gke" {
-  source                     = "terraform-google-modules/kubernetes-engine/google"
-  version                    = "4.1.0"
-  project_id                 = var.project_id
-  region                     = var.region
-  zones                      = var.zones
-  name                       = var.name
-  network                    = "default"
-  subnetwork                 = "default"
-  ip_range_pods              = ""
-  ip_range_services          = ""
-  http_load_balancing        = false
-  horizontal_pod_autoscaling = true
-  kubernetes_dashboard       = true
-  network_policy             = true
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
 
-  node_pools = [
-    {
-      name               = "default-node-pool"
-      machine_type       = var.machine_type
-      min_count          = var.min_count
-      max_count          = var.max_count
-      disk_size_gb       = var.disk_size_gb
-      disk_type          = "pd-standard"
-      image_type         = "COS"
-      auto_repair        = true
-      auto_upgrade       = true
-      service_account    = var.service_account
-      preemptible        = false
-      initial_node_count = var.initial_node_count
-    },
-  ]
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
 
-  node_pools_oauth_scopes = {
-    all = []
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
+}
 
-    default-node-pool = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-  }
+# GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
 
-  node_pools_labels = {
-    all = {}
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
-    default-node-pool = {
-      default-node-pool = true
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+
+  master_auth {
+    username = var.gke_username
+    password = var.gke_password
+
+    client_certificate_config {
+      issue_client_certificate = false
     }
-  }
-
-  node_pools_metadata = {
-    all = {}
-
-    default-node-pool = {
-      node-pool-metadata-custom-value = "my-node-pool"
-    }
-  }
-
-  node_pools_taints = {
-    all = []
-
-    default-node-pool = [
-      {
-        key    = "default-node-pool"
-        value  = true
-        effect = "PREFER_NO_SCHEDULE"
-      },
-    ]
-  }
-
-  node_pools_tags = {
-    all = []
-
-    default-node-pool = [
-      "default-node-pool",
-    ]
   }
 }
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${google_container_cluster.primary.name}-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    # preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+
+
+# # Kubernetes provider
+# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
+# # It references the variables and resources provisioned in this file. 
+# # We recommend you put this in another file -- so you can have a more modular configuration.
+# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
+# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
+
+# provider "kubernetes" {
+#   load_config_file = "false"
+
+#   host     = google_container_cluster.primary.endpoint
+#   username = var.gke_username
+#   password = var.gke_password
+
+#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+#   client_key             = google_container_cluster.primary.master_auth.0.client_key
+#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+# }
---
+++ gke-tf/terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "gman-289801"
+region     = "us-central1"
