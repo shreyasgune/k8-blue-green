fd178f83e117230c619beb11fb70fa70e7284251
shreyasgune <sgune@protonmail.com>
2021-05-03 23:05:51 -0400

    BLIZZ-gke - minor edit 19

--- gke-tf/versions.tf
+++ gke-tf/versions.tf
@@ -1,12 +1,6 @@
   
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "3.52.0"
-      credentials = file("creds.json")
-    }
+provider google {
+    source  = "hashicorp/google"
+    version = "3.52.0"
+    credentials = file("creds.json")
   }
-
-  required_version = "~> 0.14"
-}
